19d7ea50659ae839d7045e10d0d20a9b3d7ece60
shixiaopeng <shixiaopeng@sunlands.com>
2019-04-23 19:50:24 +0800

    some change

--- desk-mate-common/src/main/java/com/sunlands/deskmate/client/TzUserFriendService.java
+++ desk-mate-common/src/main/java/com/sunlands/deskmate/client/TzUserFriendService.java
@@ -1,15 +1,9 @@
 package com.sunlands.deskmate.client;
 
-import com.sunlands.deskmate.config.OauthFeignConfig;
 import com.sunlands.deskmate.vo.TzUserFriendInfo;
-import com.sunlands.deskmate.vo.UsersVO;
 import com.sunlands.deskmate.vo.response.BusinessResult;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
